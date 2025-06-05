31d41db3e07ec1ab50d8f05e56f38a3994988ab4
CamilleStocq <95492172+CamilleStocq@users.noreply.github.com>
2025-06-05 10:01:41 +0200

    Add files via upload

---
+++ exo_Monopoly/Joueur.cs
@@ -0,0 +1,48 @@
+﻿using exo_Monopoly.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exo_Monopoly
+{
+    internal class Joueur
+    {
+        public string nom;
+        public Pions pion;
+        private int position = 0;
+
+        public int positon
+        {
+            get => position;
+        }
+
+        public int Solde
+        {
+            get;
+            private set;
+        }
+
+        public bool Avancer()
+        {
+            int[] resultatDe = De.Lancer(2);
+
+            int somme = resultatDe[0] + resultatDe[1];
+            position += somme;
+
+            if (resultatDe[0] == resultatDe[1])
+            {
+                Console.WriteLine("Vous avez obtenu un double !!! Relancer le dé.");
+            }
+            else
+            {
+                Console.WriteLine($"le total du lancer est {somme}, vous pouvez donc avancer de {somme} cases. ");
+            }
+
+            return resultatDe[0] == resultatDe[1];
+
+        }
+    }
+
+}
---
+++ exo_Monopoly/Program.cs
@@ -0,0 +1,45 @@
+﻿using exo_Monopoly.Enums;
+
+namespace exo_Monopoly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entrez votre nom");
+            string userName = Console.ReadLine();
+
+            //Pions p = Pions.Dino;
+            Console.WriteLine($"Creation d'un joueur. Choisir un pion: ");
+            string[] pionNames = Enum.GetNames<Pions>();
+
+            foreach (string pionName in pionNames)
+            {
+                Console.WriteLine($"\t- {pionName}");
+            }
+
+            Console.WriteLine($"Quel pion choisissez vous ? ");
+            string userInput = Console.ReadLine();
+
+            Pions choice = Enum.Parse<Pions>(userInput);
+
+            Joueur j1 = new Joueur()
+            {
+                nom = userName,
+                pion = choice
+            };
+
+            Console.WriteLine($"Le joueur {j1.nom} avec le pion {j1.pion} se trouve à la case {j1.position}.");
+
+            bool isDouble = j1.Avancer();
+            if (isDouble)
+            {
+                Console.WriteLine("Vous avez obtenu un double !!! Relancer le dé.");
+            }
+
+            Console.WriteLine($"Le joueur {j1.nom} avec le pion {j1.pion} se trouve à la case {j1.position}.");
+
+
+        }
+    }
+}
