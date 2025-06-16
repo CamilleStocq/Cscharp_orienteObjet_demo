f433ed55dbcb5645d3014f6711e798a4d3dab976
CamilleStocq <95492172+CamilleStocq@users.noreply.github.com>
2025-06-16 10:33:58 +0200

    demo polymorphisme

---
+++ demo_polymorphisme/Joueur.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo_polymorphisme
+{
+    internal class Joueur
+    {
+        private List<InventaireItem> _inventaire;
+
+        public InventaireItem[] InventaireItems 
+        { 
+            get
+            {
+                return _inventaire.ToArray(); // pas de set car si on en met un c'est pour dire de changer tout l'inventaire d'un coup mais n veut pas ca donc pas de set
+            }
+        } 
+        public int Solde { get;set; }
+        
+        public Joueur()
+        {
+            Solde = 1000;
+            _inventaire = new List<InventaireItem>();
+        }
+
+        public void Loot (InventaireItem objet)
+        {
+            _inventaire.Add(objet);
+        }
+
+        public bool JeterObjet(InventaireItem objet)
+        {
+           return _inventaire.Remove(objet); 
+        }
+         
+    }
+}
---
+++ demo_polymorphisme/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace demo_polymorphisme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Joueur player = new Joueur();
+
+            InventaireItem pepite = new InventaireItem("Pépite", 5000);
+
+            player.Loot (pepite);
+
+            Console.WriteLine("Inventaire :");
+
+            foreach( InventaireItem item in player.InventaireItems)
+            {
+                Console.WriteLine($"\t {item.Nom}");
+            }
+
+            player.InventaireItems[0].Vendre(player);
+            Console.WriteLine($"votre solde est de {player.Solde}");
+        }
+    }
+}
